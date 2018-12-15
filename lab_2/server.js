bf03e55b99976adf0ba49eaffadad346cbd0ea74
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-12-15 12:54:31 +0300

    Fixed port

--- lab_2/server.js
+++ lab_2/server.js
@@ -11,4 +11,4 @@ app.use("/static", express.static(path.join(__dirname, "public")));
 app.set('view engine', 'pug');
 app.use(routes);
 app.use(errorHandler);
-app.listen(3000 | process.env.PORT);
+app.listen(process.env.PORT || 3000);
